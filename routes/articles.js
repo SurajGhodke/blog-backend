3bcd4f481c2bc0e746dabcf8dacb1f273b3f50d7
Suraj Ghodke <surajghodke0102@gmail.com>
2021-01-17 13:31:44 +0530

    add new article

--- routes/articles.js
+++ routes/articles.js
@@ -2,10 +2,25 @@ const express = require("express");
 const router = express.Router();
 const Articles = require("../models/article");
 
+//Get all articles
 router.get("/", (req, res) => {
   Articles.find()
     .then((article) => res.json(article))
-    .catch((err) => res.status(400).res.json(`Error: ${err}`));
+    .catch((err) => res.status(400).json(`Error: ${err}`));
+});
+
+// add new article
+router.post("/add", (req, res) => {
+  const newArticle = new Articles({
+    title: req.body.title,
+    article: req.body.article,
+    author: req.body.author,
+  });
+
+  newArticle
+    .save()
+    .then(() => res.json(" Hola.. new Article posted successfuly!"))
+    .catch((err) => res.status(400).json(`Error: ${err}`));
 });
 
 module.exports = router;
